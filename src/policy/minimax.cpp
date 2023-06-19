11670ba2b1a9fb3c3978245bcd35f73af0fa5c5c
zhiyun1101 <136875012+zhiyun1101@users.noreply.github.com>
2023-06-19 14:04:27 +0800

    Add minimax policy

---
+++ src/policy/minimax.cpp
@@ -0,0 +1,63 @@
+#include <cstdlib>
+#include <set>
+#include <climits>
+
+#include "../state/state.hpp"
+#include "./minimax.hpp"
+
+enum minimaxop
+{
+    MAXimize,
+    MINimize,
+};
+
+/**
+ * @brief get a legal action by minimax
+ * 
+ * @param state Now state
+ * @param depth You may need this for other policy
+ * @return Move 
+ */
+
+int minimax(State* root, int depth, int op)
+{
+    if(!root->legal_actions.size()) root->get_legal_actions();
+    if(depth == 0 || root->legal_actions.empty())
+    {
+        return root->evaluate();
+    }
+    if(op == MAXimize)
+    {
+        int value = -INT_MAX;
+        for(auto move: root->legal_actions)
+        {   
+            value = std::max(value, minimax(root->next_state(move), depth-1, MINimize));
+        }
+        return value;
+    }
+    else //op == MINimize
+    {
+        int value = INT_MAX;
+        for(auto move: root->legal_actions)
+        {
+            value = std::min(value, minimax(root->next_state(move), depth-1, MAXimize));
+        }
+       return value;
+    }
+}
+
+Move Minimax::get_move(State* state, int depth)
+{
+    int rootvalue = -INT_MAX;
+    Move minimaxmove;
+    for(auto move: state->legal_actions)
+    {   
+        int childvalue = minimax(state->next_state(move), depth-1, MAXimize);
+        if(childvalue > rootvalue)
+        {
+            rootvalue = childvalue;
+            minimaxmove = move;
+        }
+    }
+    return minimaxmove;
+}
---
+++ src/policy/minimax.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "../state/state.hpp"
+#include <queue>
+#include <set>
+
+/**
+ * @brief Policy class for minimax policy, 
+ * your policy class should have get_move method
+ */
+
+class Minimax
+{
+    public:
+    static Move get_move(State *state, int depth);
+};
