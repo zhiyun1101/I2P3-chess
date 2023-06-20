18856b1202ff51063e5d65d7e419cb678cdbd6af
zhiyun1101 <136875012+zhiyun1101@users.noreply.github.com>
2023-06-20 20:52:45 +0800

    Add Submission

---
+++ src/policy/submission.cpp
@@ -0,0 +1,81 @@
+#include <cstdlib>
+#include <set>
+#include <climits>
+
+#include "../state/state.hpp"
+#include "./submission.hpp"
+
+enum minimaxop
+{
+    MAXimize,
+    MINimize,
+};
+
+/**
+ * @brief get a legal action by submission
+ * 
+ * @param state Now state
+ * @param depth You may need this for other policy
+ * @return Move 
+ */
+
+int submission(State* root, int depth, int alpha, int beta, int op, int selfplayer)
+{
+    if(root->player == selfplayer && root->game_state == WIN)
+    {
+        return INT_MAX; 
+    }
+    if(root->player != selfplayer && root->game_state == WIN)
+    {
+        return -INT_MAX; 
+    }
+
+    if(!root->legal_actions.size()) root->get_legal_actions();
+    
+    if(depth == 0 || !root->legal_actions.size())
+    {
+        return root->evaluate(selfplayer);
+    }
+    if(op == MAXimize)
+    {
+        int value = -INT_MAX; 
+        for(auto move: root->legal_actions)
+        {   
+            value = std::max(value, submission(root->next_state(move), depth-1, alpha, beta, MINimize, selfplayer));
+            alpha = std::max(value, alpha);
+            if(alpha >= beta) break;
+        }        
+        return value;
+    }
+    else //op == MINimize
+    {
+        int value = INT_MAX; 
+        for(auto move: root->legal_actions)
+        {
+            value = std::min(value, submission(root->next_state(move), depth-1, alpha, beta, MAXimize, selfplayer));
+            beta = std::min(value, beta);
+            if(alpha >= beta) break;
+        }
+       return value;
+    }
+}
+
+Move Submission::get_move(State* state, int depth)
+{
+    Move minimaxmove;
+    int rootvalue = -INT_MAX; 
+
+    if(!state->legal_actions.size()) state->get_legal_actions();
+
+    for(auto move: state->legal_actions)
+    {   
+        int childvalue = submission(state->next_state(move), depth-1, -INT_MAX, INT_MAX, MINimize, state->player);
+        if(childvalue > rootvalue)
+        {
+            rootvalue = childvalue;
+            minimaxmove = move;
+        }
+    }
+
+    return minimaxmove;
+}
