7b14f3eb0ca6bf3ac484239e11a46cbcc5ba21a4
zhiyun1101 <136875012+zhiyun1101@users.noreply.github.com>
2023-06-20 21:06:21 +0800

    Add alphabeta modify

--- src/policy/alphabeta.cpp
+++ src/policy/alphabeta.cpp
@@ -12,7 +12,7 @@ enum minimaxop
 };
 
 /**
- * @brief get a legal action by alphbeta
+ * @brief get a legal action by submission
  * 
  * @param state Now state
  * @param depth You may need this for other policy
@@ -21,37 +21,32 @@ enum minimaxop
 
 int alphabeta(State* root, int depth, int alpha, int beta, int op, int selfplayer)
 {
-    if(root->player == selfplayer && root->game_state == WIN)
-    {
-        return INT_MAX-1; 
-    }
-    if(root->player != selfplayer && root->game_state == WIN)
-    {
-        return -INT_MAX+1; 
-    }
-
+    if(root->player == selfplayer && root->game_state == WIN) return INT_MAX-1; 
+    if(root->player != selfplayer && root->game_state == WIN) return -INT_MAX+1; 
     if(!root->legal_actions.size()) root->get_legal_actions();
-    if(depth == 0) return root->evaluate(selfplayer);
-    if(!root->legal_actions.size()) return root->evaluate(selfplayer);
+    if(depth == 0 || !root->legal_actions.size())
+    {
+        return root->evaluate(selfplayer);
+    }
     if(op == MAXimize)
     {
-        int value = -INT_MAX+1;
+        int value = -INT_MAX+1; 
         for(auto move: root->legal_actions)
         {   
             value = std::max(value, alphabeta(root->next_state(move), depth-1, alpha, beta, MINimize, selfplayer));
-            alpha = std::max(alpha, value);
+            alpha = std::max(value, alpha);
             if(alpha >= beta) break;
-        }
+        }        
         return value;
     }
     else //op == MINimize
     {
-        int value = INT_MAX-1;
+        int value = INT_MAX-1; 
         for(auto move: root->legal_actions)
         {
             value = std::min(value, alphabeta(root->next_state(move), depth-1, alpha, beta, MAXimize, selfplayer));
-            beta = std::min(beta, value);
-            if(beta <= alpha) break;
+            beta = std::min(value, beta);
+            if(alpha >= beta) break;
         }
        return value;
     }
@@ -59,22 +54,21 @@ int alphabeta(State* root, int depth, int alpha, int beta, int op, int selfplaye
 
 Move Alphabeta::get_move(State* state, int depth)
 {
-    Move alphabetamove;
-    int rootalpha = -INT_MAX;
+    Move minimaxmove;
+    int rootalpha = -INT_MAX; 
     //int rootbeta = INT_MAX; 
 
     if(!state->legal_actions.size()) state->get_legal_actions();
 
     for(auto move: state->legal_actions)
     {   
-        int childvalue = alphabeta(state->next_state(move), depth-1, INT_MIN, INT_MAX, MINimize, state->player);
+        int childvalue = submission(state->next_state(move), depth-1, -INT_MAX+1, INT_MAX-1, MINimize, state->player);
         if(childvalue > rootalpha)
         {
             rootalpha = childvalue;
-            alphabetamove = move;
+            minimaxmove = move;
         }
-        //if(rootalpha >= rootbeta) break;
     }
 
-    return alphabetamove;
+    return minimaxmove;
 }
